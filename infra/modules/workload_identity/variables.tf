343603b2f07654c1f2360149ef206fef47f2e3e2
kazu24z <progra.taro2021@gmail.com>
2025-05-03 21:38:15 +0900

    workload identity poolにsa追加

--- infra/modules/workload_identity/variables.tf
+++ infra/modules/workload_identity/variables.tf
@@ -6,6 +6,10 @@ variable "project_number" {
   description = "GCPプロジェクト番号"
 }
 
-variable "service_account" {
+variable "service_account_infra" {
+  description = "GitHub Actionsが利用するSA"
+}
+
+variable "service_account_app" {
   description = "GitHub Actionsが利用するSA"
 }
